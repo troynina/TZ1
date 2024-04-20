7d853c5259b2d363fda04d7dc782b547d6063dc8
troynina <troy333nina@gmail.com>
2024-04-20 16:14:54 +0300

    Change file

--- script.sh
+++ script.sh
@@ -4,12 +4,20 @@ recursia() {
     local from_dir="$1"
     local to_dir="$2"
     local file
+    local to_file
 
     for file in "$from_dir"/*; do
         if [ -d "$file" ]; then
             recursia "$file" "$to_dir"
         elif [ -f "$file" ]; then
-            cp "$file" "$to_dir"
+            local filename=$(basename "$file")
+            to_file="$to_dir/$filename"
+            local index=1
+            while [ -e "$to_file" ]; do
+                to_file="$to_dir/${filename%.*}_$index.${filename##*.}"
+                ((index++))
+            done
+            cp "$file" "$to_file"
         fi
     done
 }
